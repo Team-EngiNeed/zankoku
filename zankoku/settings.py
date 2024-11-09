2204d32dcbb9ab1535033e4652b352690ffdde59
MashleMashleMashleMashle <nicoangeloilustre@gmail.com>
2024-11-09 14:43:38 +0800

    last fix before i eat

--- zankoku/settings.py
+++ zankoku/settings.py
@@ -25,11 +25,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", "False").lower == True
+DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
-import os
-
+# Set ALLOWED_HOSTS, defaulting to an empty list if not set
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split()
+
 
 
 
