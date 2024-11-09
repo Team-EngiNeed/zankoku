71d7225f01551d8a59c2da7c6a882d87b53ba5a9
MashleMashleMashleMashle <nicoangeloilustre@gmail.com>
2024-11-09 15:06:43 +0800

    added env to directory

--- zankoku/settings.py
+++ zankoku/settings.py
@@ -22,13 +22,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = "hotdog"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # Set ALLOWED_HOSTS, defaulting to an empty list if not set
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split()
+ALLOWED_HOSTS = []
 
 
 
